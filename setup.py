47cd9e614572d69602235d8848df54f999ba2172
Dustin <6962246+djdarcy@users.noreply.github.com>
2025-05-13 10:42:25 -0400

    Initial repository setup with RepoKit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="preserve",
+    version="0.1.0",
+    description="A new project repository",
+    author="",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+)
